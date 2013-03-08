c0a7b058cb65983a55e3de19c86b3bd6e49738e1
leem99 <mitchelllee99@gmail.com>
2013-03-08 16:17:11 -0500

    Added Description to each function so that help() can be called for it.

--- gw_data_stats.py
+++ gw_data_stats.py
@@ -22,109 +22,13 @@ def open_SSdata():
 	SDgw_cred = pd.read_csv('SDgw_cred.csv', delimiter = ',', index_col = 0, parse_dates = True)
 	return gw_wh, gw_cred, SD_wh, SD_cred, SDgw_wh, SDgw_cred
 
-# Datamap of any DataFrame
-def data_map(wh,color):
 
-	cmap = colors.ListedColormap(['white', color])
-	fig = plt.figure()
-	densityplot = fig.add_subplot(1,1,1)
-	densityplot.spy(wh, aspect = 'auto', cmap = cmap)
-	densityplot.set_xticks(range(0,np.shape(wh)[1]))	
-	densityplot.set_xticklabels(wh.columns)
-	densityplot.set_yticks(range(0,np.shape(wh)[0],750))
-	densityplot.set_yticklabels(wh.index[0:np.shape(wh)[0]:750])
-	densityplot.set_xlabel('Mains and Circuits')
-	densityplot.set_ylabel('Date and Time')
-	densityplot.set_title('GW Data map')
-	plt.show()
-
-#	Datamap of to DFs to show overlap
-def data_map_comp(wh,demdata,color1,color2):
-	import numpy as np
-	import matplotlib.pyplot as plt
-	from matplotlib import colors
-	import pandas as pd
-	cols  = list(np.sort(list(set(wh.columns) | set(demdata.columns))))
-	demdata = pd.DataFrame(demdata, index = wh.index,columns = cols)
-	wh = pd.DataFrame(wh, index = wh.index,columns = cols)
-	cmap1 = colors.ListedColormap(['white', color1])
-	cmap2 = colors.ListedColormap(['white', color2])
-	fig = plt.figure()
-	densityplot = fig.add_subplot(1,1,1)
-	densityplot.spy(demdata, aspect = 'auto',cmap = cmap2)		
-	densityplot.spy(wh, aspect = 'auto', cmap = cmap1, alpha = 0.6)
-	densityplot.set_xticks(range(0,np.shape(wh)[1]))	
-	densityplot.set_xticklabels(wh.columns)
-	densityplot.set_yticks(range(0,np.shape(wh)[0],750))
-	densityplot.set_yticklabels(wh.index[0:np.shape(wh)[0]:750])
-	densityplot.set_xlabel('Mains and Circuits')
-	densityplot.set_ylabel('Date and Time')
-	densityplot.set_title('Data Map (GW = Red, SD = Green, Both = Brown)')
-	plt.show()
-
-def make_purch_rec(cred_DF):
-	cred_ary = np.array(cred_DF);
-	r, c = np.shape(cred_ary)
-	purch_ary = np.zeros((r,c)); purch_ary[:] = np.nan
-	for jx in range(0,c):
-		lastreal = 0
-		for ix in range(0,r):
-			diffs = cred_ary[ix, jx] - lastreal
-			if diffs > 0 :
-				purch_ary[ix+1,jx] = diffs
-			if np.isnan(cred_ary[ix,jx]) == False:
-				lastreal = cred_ary[ix,jx]
-	purch_ary[purch_ary < 100] = np.nan		
-	purch_ary = np.ceil(purch_ary/500.)*500.			
-	purch_rec = pd.DataFrame(purch_ary, index = cred_DF.index, columns = cred_DF.columns)
-	return purch_rec
-
-def make_typday(SDgw_wh):
-        r,c = np.shape(SDgw_wh)
-        typday = np.zeros((24, c))
-        typday_std = np.zeros((24, c))
-        for ix in range(0,24):
-            hour = SDgw_wh.index.hour
-            selector = (hour == ix)
-            typday[ix,:] = SDgw_wh[selector].mean()
-            typday_std[ix,:] = SDgw_wh[selector].std()
-
-        typday = pd.DataFrame(typday, index = range(0,24), columns = SDgw_wh.columns)
-        typday_std = pd.DataFrame(typday_std, index = range(0,24), columns = SDgw_wh.columns)
-        return typday, typday_std
-
-def make_maxday(SDgw_wh):
-        r,c = np.shape(SDgw_wh)
-        maxday = np.zeros((24, c))
-        for ix in range(0,24):
-            hour = SDgw_wh.index.hour
-            selector = (hour == ix)
-            maxday[ix,:] = SDgw_wh[selector].max()
-
-        maxday = pd.DataFrame(maxday, index = range(0,24), columns = SDgw_wh.columns)
-        return maxday
-
-def make_m_from_h(hour_data):
-	month_data = hour_data.resample('D', how = 'sum').resample('M',how = 'mean')
-	return month_data,
-	
-def make_month_bplot(month_data):
-	months = month_data.index.month
-	years = month_data.index.year
-	month_names = []
-	for ix in range(0,np.shape(month_data.index)[0]):
-		month_names.append( str(months[ix]) +'-'+ str(years[ix]))
-	fig = plt.figure()
-	month_plot = fig.add_subplot(1,1,1)
-	month_plot.set_xticks(range(0,np.shape(month_names)[0]))
-	month_plot.set_xticklabels(month_names,rotation = 90)
-	month_plot.set_title('Average Daily Energy Usage (Wh/Day)', fontsize =18)
-	month_plot.set_xlabel('Month',fontsize =18)
-	month_plot.set_ylabel('Average Daily Energy Usage (Wh/Day)',fontsize =18)
-	month_plot.bar(range(0,48), month_data, align = 'center')
-	return month_plot, month_names
 
 def data_aval_perc(gw_wh,SD_wh,SDgw_wh):
+	'''Import all of the SharedSolar gw, SD, and merged SDgw data. Determine
+	The percentage of all data that we have for SD cards, the gateway,
+	the union of these sets, and the intersection of these sets. '''
+
 	start_date = '2010-12-01 00:00:00'        
 	end_date = '2013-02-09 00:00:00'
 	gw_wh.ix[:start_date] = np.nan; SD_wh.ix[:start_date] = np.nan; SDgw_wh.ix[:start_date] = np.nan
@@ -164,11 +68,190 @@ def data_aval_perc(gw_wh,SD_wh,SDgw_wh):
 	intersect =   pd.DataFrame(pd.Series(intersect), columns = ['intersection'], dtype = float)
 	perc_avl = all_dat.join(gw).join(SD).join(union).join(intersect).sum()   
 	return perc_avl  
-        
 
 
 
-        
+# Datamap of any DataFrame
+def data_map(wh,color):
+	''' data_map(DF,color):
+	Import a DataFrame and make a map of data availability.'''
+	cmap = colors.ListedColormap(['white', color])
+	fig = plt.figure()
+	densityplot = fig.add_subplot(1,1,1)
+	densityplot.spy(wh, aspect = 'auto', cmap = cmap)
+	densityplot.set_xticks(range(0,np.shape(wh)[1]))	
+	densityplot.set_xticklabels(wh.columns)
+	densityplot.set_yticks(range(0,np.shape(wh)[0],750))
+	densityplot.set_yticklabels(wh.index[0:np.shape(wh)[0]:750])
+	densityplot.set_xlabel('Mains and Circuits')
+	densityplot.set_ylabel('Date and Time')
+	densityplot.set_title('GW Data map')
+	plt.show()
 
-        
+#	Datamap of to DFs to show overlap
+def data_map_comp(wh,demdata,color1,color2):
+	'''data_map(DF1,color1,DF2,color2):
+	Import two DataFrames and plot their overlap'''
+	Import a DataFrame and make a map of data availability
+	import numpy as np
+	import matplotlib.pyplot as plt
+	from matplotlib import colors
+	import pandas as pd
+	cols  = list(np.sort(list(set(wh.columns) | set(demdata.columns))))
+	demdata = pd.DataFrame(demdata, index = wh.index,columns = cols)
+	wh = pd.DataFrame(wh, index = wh.index,columns = cols)
+	cmap1 = colors.ListedColormap(['white', color1])
+	cmap2 = colors.ListedColormap(['white', color2])
+	fig = plt.figure()
+	densityplot = fig.add_subplot(1,1,1)
+	densityplot.spy(demdata, aspect = 'auto',cmap = cmap2)		
+	densityplot.spy(wh, aspect = 'auto', cmap = cmap1, alpha = 0.6)
+	densityplot.set_xticks(range(0,np.shape(wh)[1]))	
+	densityplot.set_xticklabels(wh.columns)
+	densityplot.set_yticks(range(0,np.shape(wh)[0],750))
+	densityplot.set_yticklabels(wh.index[0:np.shape(wh)[0]:750])
+	densityplot.set_xlabel('Mains and Circuits')
+	densityplot.set_ylabel('Date and Time')
+	densityplot.set_title('Data Map (GW = Red, SD = Green, Both = Brown)')
+	plt.show()
+
+def data_map_mag(DF, vmin, vmax):
+	'''Import DataFrame and make heatmap of data according to availablity and magnitude.
+	Specify minimum and maximum values for scaling of color
+	Resolution finer than monthly will crash.	
+	'''
+	import numpy as np
+	import pandas as pd
+	import matplotlib.pyplot as plt
+	import matplotlib as mpl
+	fig = plt.figure()
+	dayuse = fig.add_subplot(1,1,1)
+	pic = dayuse.imshow(DF, aspect = 'auto', cmap= mpl.cm.jet, vmin = vmin, vmax= vmax)
+	dayuse.set_xticks(range(0,np.shape(DF)[-1]))
+	dayuse.set_xticklabels(DF.columns)
+	dayuse.set_yticks(range(0,np.shape(DF)[0]))
+	dayuse.set_yticklabels(DF.index)
+	dayuse.set_title('Average Daily Energy Consumption over Time')
+	dayuse.set_xlabel('Location')
+	dayuse.set_ylabel('Month')
+	pic.set_interpolation('nearest')
+	fig.colorbar(pic).set_label('Average Wh Consumed Daily')
+	dayuse.plot()
+
+
+
+
+def make_maxday(SDgw_wh):
+	'''Import the hourly energy usage of a consumer or mains 	
+	and create a diurnal day of using the maximum energy consumption
+	at each hour over the entire timeseries'''
+    r,c = np.shape(SDgw_wh)
+    maxday = np.zeros((24, c))
+    for ix in range(0,24):
+        hour = SDgw_wh.index.hour
+        selector = (hour == ix)
+        maxday[ix,:] = SDgw_wh[selector].max()
+
+    maxday = pd.DataFrame(maxday, index = range(0,24), columns = SDgw_wh.columns)
+    return maxday
+
+def make_m_from_h(hour_data):
+	""" Take in a DataFrame with hourly resolution. Convert to monthly resolution
+	by first resample('D',sum) then resample('M',mean) to show the average daily energy
+	use in each month"""
+	hour_data[hour_data >= 1000] = np.nan
+	month_data = hour_data.resample('D', how = 'sum').resample('M',how = 'mean')
+	return month_data,
+	
+def make_month_bplot(month_data):
+	'''Import the hourly energy usage of a consumer or mains 
+	and make a barplot of average daily energy consumption by month'''
+	months = month_data.index.month
+	years = month_data.index.year
+	month_names = []
+	for ix in range(0,np.shape(month_data.index)[0]):
+		month_names.append( str(months[ix]) +'-'+ str(years[ix]))
+	fig = plt.figure()
+	month_plot = fig.add_subplot(1,1,1)
+	month_plot.set_xticks(range(0,np.shape(month_names)[0]))
+	month_plot.set_xticklabels(month_names,rotation = 90)
+	month_plot.set_title('Average Daily Energy Usage (Wh/Day)', fontsize =18)
+	month_plot.set_xlabel('Month',fontsize =18)
+	month_plot.set_ylabel('Average Daily Energy Usage (Wh/Day)',fontsize =18)
+	month_plot.bar(range(0,48), month_data.astype(float), align = 'center')
+	return month_plot, month_names
+
+
+def make_purch_rec(cred_DF):
+	'''make a timeseries record of purchases for call SharedSolar Consumers
+	Constructs consumer history by finding increase in credits for each 
+	consumer and rounding up to the nearest increment of 500'''
+	cred_ary = np.array(cred_DF);
+	r, c = np.shape(cred_ary)
+	purch_ary = np.zeros((r,c)); purch_ary[:] = np.nan
+	for jx in range(0,c):
+		lastreal = 0
+		for ix in range(0,r):
+			diffs = cred_ary[ix, jx] - lastreal
+			if diffs > 0 :
+				purch_ary[ix+1,jx] = diffs
+			if np.isnan(cred_ary[ix,jx]) == False:
+				lastreal = cred_ary[ix,jx]
+	purch_ary[purch_ary < 100] = np.nan		
+	purch_ary = np.ceil(purch_ary/500.)*500.			
+	purch_rec = pd.DataFrame(purch_ary, index = cred_DF.index, columns = cred_DF.columns)
+	return purch_rec
+
+
+def make_typday(SDgw_wh):
+		'''Import the hourly energy usage of a consumer or mains 	
+		and create a typical diurnal day of average energy consumption
+		and associated standard deviation'''
+        r,c = np.shape(SDgw_wh)
+        typday = np.zeros((24, c))
+        typday_std = np.zeros((24, c))
+        for ix in range(0,24):
+            hour = SDgw_wh.index.hour
+            selector = (hour == ix)
+            typday[ix,:] = SDgw_wh[selector].mean()
+            typday_std[ix,:] = SDgw_wh[selector].std()
+
+        typday = pd.DataFrame(typday, index = range(0,24), columns = SDgw_wh.columns)
+        typday_std = pd.DataFrame(typday_std, index = range(0,24), columns = SDgw_wh.columns)
+        return typday, typday_std
+
+
+def sort_country(DF):
+	''' Sort SharedSolar DF into ml_DF and ug_DF'''
+	r, c = np.shape(DF)
+	#Separate ML (dataset 1)
+	ml = []
+	ug = []
+	for ix in range(0,c):
+		n = DF.columns[ix][0]
+		if n == 'm':
+			ml.append(DF.columns[ix])
+		else:
+			ug.append(DF.columns[ix])
+
+	ml_DF = DF[ml]
+	ug_DF = DF[ug]
+
+	return ml_DF, ug_DF
+
+def sort_mains(DF):
+	''' Retun two DFs one of mains and one of circuits from original DF'''
+	mains = []
+	circs= []
+	for ix, row in enumerate(DF.columns):
+		n = int(row.split('_')[1])
+		if n == 0:
+		  	mains.append(DF.columns[ix])
+		else:
+		    circs.append(DF.columns[ix])
+	mains = DF[mains]
+	circs = DF[circs]
+	return mains, circs
+
+
 
