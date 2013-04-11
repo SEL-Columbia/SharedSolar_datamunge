92c6e30057a8195bf636b2978d2b39b11bbb541d
leem99 <mitchelllee99@gmail.com>
2013-04-11 12:18:35 -0400

    Added function for making average day DF

--- power_stats.py
+++ power_stats.py
@@ -20,8 +20,10 @@ def open_power_ts(circuit):
 	ts[ts > ts.std()*5. + ts.mean()] = np.nan
 	return ts
 
-def make_max_day_DF():
+
+def make_ave_day_DF():
 	import gw_data_stats
+	import os
 	#ts = pd.read_csv(ts_path + circuit + '_min.csv', delimiter = ';', index_col = 0, parse_dates = True)
 	#ts[ts > ts.std()*5. + ts.mean()] = np.nan
 	flist = []
@@ -33,7 +35,37 @@ def make_max_day_DF():
 			for _file in filenames:
 				flist.append(_file)
 
-	max_day_ary = pd.DataFrame(index = range(0,24))
+	ave_day_DF = pd.DataFrame(index = range(0,24))
+	stdev_day_DF = pd.DataFrame(index = range(0,24))
+	for ix, fname in enumerate(flist):
+		x = pd.read_csv(DFlist +'/' + fname, delimiter = ';', index_col = 0, parse_dates = True)
+		x[x > x.std()*5. + x.mean()] = np.nan
+		x = x.dropna()
+		col_head = string.rsplit(fname,'_',1)[0]
+		col = pd.DataFrame(gw_data_stats.make_typday(x)[0])
+		col_stdev = pd.DataFrame(gw_data_stats.make_typday(x)[1])
+		col.columns = [col_head]
+		ave_day_DF = ave_day_DF.join(pd.DataFrame(col))
+		stdev_day_DF = stdev_day_DF.join(pd.DataFrame(col))
+		print col_head
+	return ave_day_DF, stdev_day_DF
+
+
+def make_max_day_DF():
+	import gw_data_stats
+	import os
+	#ts = pd.read_csv(ts_path + circuit + '_min.csv', delimiter = ';', index_col = 0, parse_dates = True)
+	#ts[ts > ts.std()*5. + ts.mean()] = np.nan
+	flist = []
+	DFlist = '/media/mountpoint/Users/Mitchell/Documents/Shared_Solar/Shared_Solar_database/csv_flat_files_minute/DFlist'
+	#for (dirpath,dirnames,filenames) in os.walk('C:\Users\Mitchell\Documents\Shared_Solar\Shared_Solar_database'):
+	for (dirpath,dirnames,filenames) in os.walk('/media/mountpoint/Users/Mitchell/Documents/Shared_Solar/Shared_Solar_database/csv_flat_files_minute'):
+		print dirpath
+		if dirpath == DFlist:
+			for _file in filenames:
+				flist.append(_file)
+
+	max_day_DF = pd.DataFrame(index = range(0,24))
 	for ix, fname in enumerate(flist):
 		x = pd.read_csv(DFlist +'/' + fname, delimiter = ';', index_col = 0, parse_dates = True)
 		x[x > x.std()*5. + x.mean()] = np.nan
@@ -41,13 +73,15 @@ def make_max_day_DF():
 		col_head = string.rsplit(fname,'_',1)[0]
 		col = pd.DataFrame(gw_data_stats.make_maxday(x))
 		col.columns = [col_head]
-		max_day_ary = max_day_ary.join(pd.DataFrame(col))
+		max_day_DF = max_day_DF.join(pd.DataFrame(col))
 		
 		print col_head
 	return max_day_DF
 
+
 def make_ldf(site, max_day_DF):	
 	import gw_data_stats
+	import os
 	'''make load diversity factor'''
 	site_dict = gw_data_stats.make_site_dict(max_day_DF)
 	circuits = site_dict[site]	
